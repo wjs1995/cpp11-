4fd23dc48ba9e842a941d34facdc5f62b6cb205a
wangjinsheng <1334258066@qq.com>
2025-12-12 09:23:19 +0800

    优化

---
+++ main.c
@@ -0,0 +1,12 @@
+//
+// Created by wangj on 2025/12/11.
+//
+extern int gdata;
+int sum(int a, int b);
+int data = 20;
+int main() {
+    int a = gdata;
+    int b= data;
+    int ret = sum(a, b);
+    return 0;
+}
---
+++ sum.c
@@ -0,0 +1,8 @@
+//
+// Created by wangj on 2025/12/11.
+//
+
+int gdata = 10;
+int sum(int a, int b) {
+    return a + b;
+};
